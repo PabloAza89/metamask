00f55b9a1ec52309ebdb4efffdf39040d1e64713
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-12-02 13:06:50 -0300

    h

--- client/src/Metamask.js
+++ client/src/Metamask.js
@@ -1,16 +1,19 @@
-import './App.css';
-require('@metamask/legacy-web3')
-const Web3 = require('web3-eth');
+import './Metamask.css';
+require('@metamask/legacy-web3') // npm i @metamask/legacy-web3 ^2.0.0
+const Web3 = require('web3-eth'); // npm i web3-eth // ^1.8.1
 
 function App() {
   const { web3 , ethereum } = window
     
   const InitPayButton = () => {
-    // paymentAddress is where funds will be send to
-    //const paymentAddress = 'YOUR ACCOUNT ADDRESS HERE'
+    //const paymentAddress = ACA VA EL NUMERO DE CUENTA DE METAMASK, DONDE SE DIRIGE EL DINERO..
+    //                       ..DE DONDE SALE EL DINERO ES EL NRO DE CTA DE LA EXTENSION DE METAMASK..
+    //                       ..COMO EJ SALE Y SE ENVIA HACIA LA MISMA CUENTA..
+    //                       ..RECHAZO: Payment failed || EXITO: Payment successful
     const paymentAddress = '0x511cB3cB7b04319448e1a27079eE6021d0A4c7dc'
-    const amountEth = 0.0000001
-    //const amountEth = 1
+    const amountEth = 0.0000001 // VALOR REAL TIENE QUE TENER 0. + 7 DIGITOS
+                                // TRANSCACCION TIENE IMPUESTO DE 0.0000315 GoerliETH
+    //const amountEth = 1 // VALOR DE PRUEVA, VA A SER RECHAZADO
     web3.eth.sendTransaction({
       to: paymentAddress,
       value: web3.toWei(amountEth, 'ether')
@@ -35,13 +38,13 @@ function App() {
                 window.web3 = new Web3(ethereum);
                 try {
                   await ethereum.enable();
-                  //InitPayButton()
+                  //InitPayButton() // DUPLICA MOVIMIENTOS
                 } catch (err) {
                   document.getElementById('status').innerHTML = 'User denied account access'
                 }
               } else if (window.web3) {
                 window.web3 = new Web3(web3.currentProvider)
-                //InitPayButton()
+                //InitPayButton() // DUPLICA MOVIMIENTOS
               } else {
                 document.getElementById('status').innerHTML = 'No Metamask (or other Web3 Provider) installed'
               }
